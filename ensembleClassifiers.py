fda9788f1adcba73999c731c7057ee6bbec923be
Chang <dfrontier1@gmail.com>
2018-12-02 14:48:22 -0800

    have not tested yet

--- ensembleClassifiers.py
+++ ensembleClassifiers.py
@@ -3,11 +3,17 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 def main():
     df = pd.read_csv('train_csv')
-    clf = RandomForestClassifier(n=4)
-
+    x = df['Phrase'] # Features
+    y = df['Sentiment'] # Labels
+    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.3) # 70% training and 30% test
+    clf = RandomForestClassifier()
+    clf.fit(xTrain,yTrain) # Build a forest of trees from the training set (x, y).
+    yPred = clf.predict(xTest)
+    print("Accuracy:",metrics.accuracy_score(yTest, yPred))
 
 if __name__ == "__main__":
     main()
