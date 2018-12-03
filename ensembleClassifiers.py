bc5019704887e4b73ea69ee59c3f2f3b546b5d48
Bryan Tor <bterrific2008@gmail.com>
2018-12-03 04:22:03 -0800

    Started on stop words, lowercase text

--- ensembleClassifiers.py
+++ ensembleClassifiers.py
@@ -1,23 +1,35 @@
 # Chang Kim, Bryan Tor, Carson Burr
 
 import pandas as pd
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.pipeline import Pipeline
 from sklearn import metrics
+from nltk.corpus import stopwords
 
+# baseline without cleaning: 60.54679% 60.7969975% 60.8214078% 60.93735% 60.52543%
+# baseline with cleaning:    60.63222% 60.940408% 60.85192% 60.78174% 60.766484% 60.44915%
 def clean_text(text):
     import string
+    text = text.lower()
     translator = str.maketrans('','', string.punctuation)
     return text.translate(translator)
     
+    
 def main():
     df = pd.read_csv('train.csv')
-    
-    print(df.head(10))
+ 
+    for i in range(9):
+        print(df['Phrase'][i+20])   
     
     df['Phrase'] = df['Phrase'].apply(clean_text)
+    
+    print("---")
+    
+    for i in range(9):
+        print(df['Phrase'][i+20])
     
     print(df.head(10))
     
@@ -25,16 +37,15 @@ def main():
     y = df['Sentiment'] # Labels
     xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.3) # 70% training and 30% test
        
-
     clf = Pipeline([
-        ('vect', CountVectorizer()),
-        ('tfidf', TfidfTransformer()),
-        ('clf', RandomForestClassifier())
-    ])
-    clf.fit(xTrain, yTrain) # Build a forest of trees from the training set (x, y).
-    
-    yPred = clf.predict(xTest)
-    print("Accuracy:",metrics.accuracy_score(yTest, yPred))
+            ('vect', CountVectorizer()),
+            ('tfidf', TfidfTransformer()),
+            ('clf', RandomForestClassifier())
+            ])
+    #clf.fit(xTrain, yTrain) # Build a forest of trees from the training set (x, y).
+        
+    # yPred = clf.predict(xTest)
+    # print("Accuracy:",metrics.accuracy_score(yTest, yPred))
 
 if __name__ == "__main__":
     main()
