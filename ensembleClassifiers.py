b9fcf2259349399f938fd1696b21b7864c99013d
Chang <dfrontier1@gmail.com>
2018-12-03 02:34:50 -0800

    implementing soft voting and cross validation

--- ensembleClassifiers.py
+++ ensembleClassifiers.py
@@ -10,20 +10,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
 from sklearn.pipeline import Pipeline
 from sklearn import metrics
-from sklearn.preprocessing import StandardScaler  
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import RidgeClassifier
+from sklearn.model_selection import GridSearchCV
 
 def main():
     df = pd.read_csv('train.csv')
     x = df['Phrase'] # Features
     y = df['Sentiment'] # Labels
-    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.3) # 70% training and 30% test
-    clf = Pipeline([
+    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.2) # 70% training and 30% test
+    clf1 = Pipeline([
+        ('tfidf', TfidfVectorizer()),
+        ('clf', ExtraTreesClassifier())
+    ])
+    clf2 = Pipeline([
         ('tfidf', TfidfVectorizer()),
         ('clf', LinearSVC())
     ])
-    clf.fit(xTrain, yTrain) # Build a forest of trees from the training set (x, y).
-    
-    yPred = clf.predict(xTest)
+    clf3 = Pipeline([
+        ('tfidf', TfidfVectorizer()),
+        ('clf', MLPClassifier())
+    ])
+    eclf = VotingClassifier(estimators=[('rf', clf1), ('svc', clf2), ('mlp', clf3)], voting='soft')
+    grid = GridSearchCV(estimator=eclf, cv=5)
+    grid = grid.fit(xTrain,yTrain)
+    yPred = grid.predict(xTest)
     print("Accuracy:",metrics.accuracy_score(yTest, yPred))
 
 if __name__ == "__main__":
