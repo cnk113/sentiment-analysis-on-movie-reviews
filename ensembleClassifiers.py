82f869d48155603c24a96b8167650ce3119acf38
Bryan Tor <bterrific2008@gmail.com>
2018-12-04 12:30:08 -0800

    turns out a lot of this was unneccesary who could have guessed

--- ensembleClassifiers.py
+++ ensembleClassifiers.py
@@ -10,14 +10,7 @@ from sklearn import metrics
 import time
 from nltk.corpus import stopwords
 
-# baseline without cleaning: 60.54679% 60.7969975% 60.8214078% 60.93735% 60.52543%
-# baseline with cleaning:    60.63222% 60.940408% 60.85192% 60.78174% 60.766484% 60.44915%
-def clean_text(text):
-    import string
-    text = text.lower()
-    translator = str.maketrans('','', string.punctuation)
-    return text.translate(translator)
-    
+   
     
 def main():
     df = pd.read_csv('train.csv')
@@ -27,11 +20,7 @@ def main():
     
     stop = stopwords.words('english')
     
-    df['Phrase'] = df['Phrase'].apply(clean_text)
-    # df["test"] = df["Phrase"].apply(nltk.word_tokenize)
-    # df['Phrase'] = df['test'].apply(lambda x: [item for item in x if item not in stop])
-    
-    stop.remove("not")
+    # stop.remove("not")
     print(stop)
     
     # print("---")
@@ -44,18 +33,18 @@ def main():
     xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.3) # 70% training and 30% test
     
     start = time.time()
-    print("Training Starts at: ", start)
+    print("Training Starts at: ", time.strftime('%X %x %Z'))
     
     clf = Pipeline([
-            ('vect', CountVectorizer(stop_words = stop)), # stop_words = stop
+            ('vect', CountVectorizer(stop_words = stop, ngram_range=(1, 3))), # stop_words = stop
             ('tfidf', TfidfTransformer()),
             ('clf', RandomForestClassifier())
             ])
-    #clf.fit(xTrain, yTrain) # Build a forest of trees from the training set (x, y).
+    clf.fit(xTrain, yTrain) # Build a forest of trees from the training set (x, y).
         
     print("Time Took: ", (time.time() - start))
     
-    #yPred = clf.predict(xTest)
+    yPred = clf.predict(xTest)
     print("Accuracy:",metrics.accuracy_score(yTest, yPred))
 
 if __name__ == "__main__":
