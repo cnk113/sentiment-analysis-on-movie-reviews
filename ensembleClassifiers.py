f36bb58bbf27d46e69b7985767999715876987ff
carsonburr <carsonwburr@gmail.com>
2018-12-04 14:08:32 -0800

    added min_df and ngram_range parameters for 63.74% accuracy

--- ensembleClassifiers.py
+++ ensembleClassifiers.py
@@ -17,14 +17,28 @@ def main():
     x = df['Phrase'] # Features
     y = df['Sentiment'] # Labels
     xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.3) # 70% training and 30% test
+    # vec = CountVectorizer(
+    #         min_df=0.01,
+    #         ngram_range=(1,2))
+    # vec.fit(x)
+    # print(vec.vocabulary_)
     clf = Pipeline([
-        ('tfidf', TfidfVectorizer()),
+        ('tfidf', TfidfVectorizer(
+            min_df=2,
+            ngram_range=(1,2))),
         ('clf', LinearSVC())
     ])
     clf.fit(xTrain, yTrain) # Build a forest of trees from the training set (x, y).
     
     yPred = clf.predict(xTest)
-    print("Accuracy:",metrics.accuracy_score(yTest, yPred))
+    # print("Accuracy:",metrics.accuracy_score(yTest, yPred))
+    return metrics.accuracy_score(yTest, yPred)
 
 if __name__ == "__main__":
-    main()
+    sum = 0
+    trials = 10
+    for i in range(trials):
+        sum += main()
+    avg = sum / trials
+    print(avg)
+    # main()
