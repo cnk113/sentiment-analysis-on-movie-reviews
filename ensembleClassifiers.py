831c664086c8b70fc112c5e668d0d3e3196afa49
Chang Kim <dfrontier1@gmail.com>
2018-12-05 11:53:42 -0800

    its working now

--- ensembleClassifiers.py
+++ ensembleClassifiers.py
@@ -1,12 +1,7 @@
 # Chang Kim, Bryan Tor, Carson Burr
 
 import pandas as pd
-from sklearn.svm import LinearSVC
-from sklearn.neural_network import MLPClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn import metrics
+import numpy as np
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.models import Sequential
@@ -20,34 +15,32 @@ def main():
     df = pd.read_csv('train.csv')
     x = df['Phrase'] # Features
     y = df['Sentiment'] # Labels
-    xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size=0.3) # 80% training and 20% test
-    tokenizer = Tokenizer(num_words=2000)
+    tokenizer = Tokenizer(num_words=400)
     tokenizer.fit_on_texts(df['Phrase'].values)
     X = tokenizer.texts_to_sequences(df['Phrase'].values)
-    X = pad_sequences(X, maxlen=2000)
+    max_words = 400
     embed_dim = 128
     lstm_out = 196
-    num_words = 2000
-    ckpt_callback = ModelCheckpoint('keras_model', 
-                                 monitor='val_loss', 
-                                 verbose=1, 
-                                 save_best_only=True, 
-                                 mode='auto')
+    X = pad_sequences(X, max_words)
     model = Sequential()
-    model.add(Embedding(num_words, embed_dim, input_length = X.shape[1]))
+    model.add(Embedding(max_words, embed_dim, input_length = X.shape[1]))
     model.add(LSTM(lstm_out, recurrent_dropout=0.2, dropout=0.2))
-    model.add(Dense(9,activation='softmax'))
-    model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics = ['categorical_crossentropy'])
+    model.add(Dense(5,activation='softmax'))
+    model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics = ['acc'])
     print(model.summary())
-    batch_size = 32 
-    model.fit(xTrain, yTrain, epochs=8, batch_size=batch_size, validation_split=0.2, callbacks=[ckpt_callback])
-    model = load_model('keras_model')
-    probas = model.predict(X_test)
+    batch_size = 32
+    model.fit(X, pd.get_dummies(y).values, epochs=8, batch_size=batch_size, validation_split=0.2)
+    model.save('model')
+    model = load_model('model')
+    df2 = pd.read_csv('testset_1.csv')
+    xTest = df2['Phrase']
+    yTest = df2['Sentiment']
+    probas = model.predict(xTest)
     pred_indices = np.argmax(probas, axis=1)
-    classes = np.array(range(1, 10))
+    classes = np.array(range(5))
     preds = classes[pred_indices]
-    print('Log loss: {}'.format(log_loss(classes[np.argmax(Y_test, axis=1)], probas)))
-    print('Accuracy: {}'.format(accuracy_score(classes[np.argmax(Y_test, axis=1)], preds)))
+    print('Log loss: {}'.format(log_loss(classes[np.argmax(yTest, axis=1)], probas)))
+    print('Accuracy: {}'.format(accuracy_score(classes[np.argmax(yTest, axis=1)], preds)))
 
 if __name__ == "__main__":
     main()
